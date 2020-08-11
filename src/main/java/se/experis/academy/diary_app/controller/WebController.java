e1d374ce426d8330d18d39f51eb8886424a97080
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-11 16:01:44 +0200

    feat: Added redirection to the index page It still needs to get data from the Repository

--- src/main/java/se/experis/academy/diary_app/controller/WebController.java
+++ src/main/java/se/experis/academy/diary_app/controller/WebController.java
@@ -1,4 +1,23 @@
 package se.experis.academy.diary_app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Class that handles all the views
+ */
+@Controller
 public class WebController {
+
+
+    /**
+     * Gets contacts and returns them to index.html
+     * @param model
+     * @return to index.html with list of contacts
+     */
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
 }
